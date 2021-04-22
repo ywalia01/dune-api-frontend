1d7f3d120b14807881f92111ebc6c9e6cbee4b90
Yash Walia <yashwalia@Yashs-MacBook-Air.local>
2021-04-22 16:12:53 +0530

    define routes

---
+++ src/Routes/Docs.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import About from '../components/ui/About';
+function Docs() {
+    return (
+        <div className="container">
+            <About/>
+        </div>
+    )
+}
+
+export default Docs
---
+++ src/Routes/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import About from '../components/ui/About';
+import Support from '../components/ui/Support';
+
+function Home() {
+  return (
+    <div className="container">
+        <About/>
+        <Support/>
+    </div>
+  );
+}
+
+export default Home;
